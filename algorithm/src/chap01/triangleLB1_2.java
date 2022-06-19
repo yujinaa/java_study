f60ab4672e7def68216a908ec5c75cdb27e1f96c
yujinaa <olim920@naver.com>
2022-06-20 00:56:40 +0900

    이중반복문으로 왼쪽 아래가 직각인 이등변 삼각형 만들기

---
+++ algorithm/src/chap01/triangleLB1_2.java
@@ -0,0 +1,24 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class triangleLB1_2 {
+	// 왼쪽 아래가 직각인 이등변삼각형을 출력하는 메서드를 작성하세요
+	static void triangleLB(int n) {
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n;
+		System.out.println("왼쪽 아래가 직각인 이등변 삼각형을 출력 ");
+		System.out.print("몇 단 삼각형입니까? : ");
+		n = input.nextInt();
+		triangleLB(n);
+	}
+}
+
